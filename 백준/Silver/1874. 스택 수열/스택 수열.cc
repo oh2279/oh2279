88d5707184be8e8532583c8bc324f7d7b908d534
oh2279 <85153493+oh2279@users.noreply.github.com>
2022-04-28 17:25:24 +0900

    [Silver III] Title: 스택 수열, Time: 20 ms, Memory: 2624 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stack>
+#include<string>
+using namespace std;
+
+
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	bool flag;
+	int n, last = 0;
+	cin >> n;
+	string ans;
+	stack<int> s;
+	for (int i = 0; i < n; i++) {
+		int num;
+		cin >> num;
+		if (num > last) {
+			while (num > last) {
+				s.push(++last);
+				ans += "+";
+			}
+			s.pop();
+			ans += '-';
+		}
+		else {
+			flag = false;
+			if (!s.empty()) {
+				if (num == s.top()) {
+					flag = true;
+				}
+				s.pop();
+				ans += '-';
+			}
+			if (!flag) {
+				cout << "NO\n";
+				return 0;
+			}
+		}
+	}
+	
+
+		for (auto x : ans) {
+			cout << x << '\n';
+		}
+	
+}
