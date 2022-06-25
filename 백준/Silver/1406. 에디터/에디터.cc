c111d38a1a6b6ad7b5e746c966625b4e59715451
oh2279 <85153493+oh2279@users.noreply.github.com>
2022-06-26 00:06:54 +0900

    [Silver II] Title: 에디터, Time: 72 ms, Memory: 21020 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.cc
+++ 백준/Silver/1406. 에디터/에디터.cc
@@ -1,58 +1,68 @@
-#include <iostream>
-#include <list>
-#include <string>
+#include<iostream>
+#include<algorithm>
+#include<list>
+#include<string>
 
 using namespace std;
 
-int main(int argc, char**argv) {
-	
-	ios::sync_with_stdio(0);
+list<char> li;
+list<char>::iterator iter ; //반복자 선언
+
+int main() {
+	ios_base::sync_with_stdio(0);
 	cin.tie(0);
-	
-	string str;
-	list<char> L;
+	cout.tie(0);
 
-	cin >> str;
-	
-	for(int i=0; i<str.size(); i++){
-		L.push_back(str.at(i));
+	string s;
+	char instruction, ch;
+	int n;
+
+	cin >> s >> n;
+
+	for (int i = 0; i < s.length(); i++) {
+		li.push_back(s[i]);
 	}
-		
-	int N;
-	list<char>::iterator cur = L.end();
-	
-	cin >> N;
-	
-		
-	for(int i=0; i<N; i++){
-		
-		char c;
-		cin >> c;
-		
-		if(c=='L'){
-			if(cur!=L.begin()) cur--;
-		}			
+	iter = li.end();
+
+	while (n--) {
+		cin >> instruction;
+		if (instruction == 'P') {
+			cin >> ch;
+			if (iter == li.end()) { // 맨 뒤면 맨 뒤에 삽입
+				li.push_back(ch);
+				iter = li.end();
+			}
+			else { // 아니면 중간에 삽입
+				li.insert(iter, ch);
+				//iter++;
+			}
 			
-		else if(c=='D'){
-			if(cur!=L.end()) cur++;
 		}
-			
-		else if(c=='B'){
-			if(cur!=L.begin())
-				cur = L.erase(--cur);
+		else if (instruction == 'L') { // 맨 앞 아니면 커서 왼쪽으로 이동시킴
+			if (iter != li.begin()) {
+				iter--;
+			}
 		}
-		
-		else if(c=='P'){
-			char s;
-			cin >> s;
-			L.insert(cur,s);
+		else if (instruction == 'D') { // 맨 뒤 아니면 커서 오른쪽으로 이동시킴
+			if (iter != li.end()) {
+				iter++;
+			}
 		}
-		
+		else if (instruction == 'B') {
+			if (iter == li.end()) { // 커서가 맨 오른쪽이면 맨 뒤 삭제
+				li.pop_back();
+				// iter = li.end();
+			}
+			else if (iter == li.begin()) { // 커서가 맨 앞이면 무시
+				continue;
+			}
+			else { // 맨 앞, 맨 뒤 모두 아니면 커서 왼쪽의 문자 삭제
+				iter--;
+				iter = li.erase(iter);
+			}
+		}
 	}
-
-	for(list<char>::iterator it=L.begin(); it!=L.end(); it++){
-		cout << *it;
+	for (iter = li.begin(); iter != li.end(); iter++) {
+		cout << *iter;
 	}
-
-	return 0;
 }
