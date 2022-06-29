54d2431ed6f270b74af1f406db6884e4d71cc7bd
oh2279 <85153493+oh2279@users.noreply.github.com>
2022-06-30 00:14:08 +0900

    [Bronze II] Title: 입실 관리, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/5524. 입실 관리/입실 관리.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cctype>   //isupper를 쓰기위한 헤더파일
+#include <string>   
+ 
+using namespace std;
+ 
+int main() {
+ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);        
+ 
+    string s;
+    int n;
+    cin>>n;
+ 
+    for(int i=0; i<n; i++){
+ 
+        cin>>s;
+ 
+        for(int i=0; i<s.length(); i++){
+            if(isupper(s[i])){     
+                s[i]=tolower(s[i]); //문자열에 대문자가 있으면 tolower:소문자로 바꿔라
+            }
+        }
+            cout<<s<<"\n";
+    }
+}
