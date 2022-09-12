96e8debed8c13e4fd8aead4af93ac231c8c8a128
oh2279 <85153493+oh2279@users.noreply.github.com>
2022-09-13 01:02:45 +0900

    [Gold III] Title: 다리 만들기, Time: 4 ms, Memory: 2116 KB -BaekjoonHub

--- 백준/Gold/2146. 다리 만들기/다리 만들기.cc
+++ 백준/Gold/2146. 다리 만들기/다리 만들기.cc
@@ -1,127 +1,117 @@
-#include <iostream>
-#include <stdio.h>
-#include <math.h>
-#include <bitset>
-#include <cstring>
-#include <vector>
-#include <queue>
-#include <algorithm>
-#define MAX 101
-#define INF 987654321
+#include <bits/stdc++.h>
+#define ll long long
 using namespace std;
-typedef pair<int, int> node;
 
-int n,ans,cnt;
-int map[MAX][MAX];
-int area[MAX][MAX];
-bool visited[MAX][MAX];
+int n,cnt,ans;
+int board[101][101];
+int dist[101][101];
+bool visited[101][101];
+int dx[]={1,0,-1,0};
+int dy[]={0,1,0,-1};
 
-int dx[4] = {0,1,0,-1};
-int dy[4] = {1,0,-1,0};
+void bfs(int a, int b){
+    queue<pair<int,int>> q;
+    dist[a][b]=cnt;
+    q.push({a,b});
 
-void bfs_area(int r,int c){
-    queue<node> q;
-    q.push(node(r,c));
-    area[r][c] = cnt;
-    
-    while (!q.empty()) {
+    while(!q.empty()){
         int x = q.front().first;
         int y = q.front().second;
         q.pop();
-        
+
         for(int i=0; i<4; i++){
             int nx = x + dx[i];
             int ny = y + dy[i];
-            
-            if(nx<0 || nx>=n || ny<0 || ny>=n) continue;
-            
-            if(area[nx][ny]==-1 && map[nx][ny]==1){
-                q.push(node(nx,ny));
-                area[nx][ny] = cnt;
+
+            if(nx >= 0 && nx < n && ny >=0 && ny < n && dist[nx][ny]==-1 && board[nx][ny]==1){
+                q.push({nx,ny});
+                dist[nx][ny]=cnt;
             }
         }
     }
 }
 
-bool is_adjacentSea(int x,int y){
-    for(int i=0; i<4; i++){
-        int nx = x + dx[i];
-        int ny = y + dy[i];
-        
-        if(nx<0 || nx>=n || ny<0 || ny>=n) continue;
-        
-        if(map[nx][ny]==0) return true;
-    }
-    return false;
-}
-
-void bfs_dist(int r,int c){
+void bfs2(int a, int b) {
+    queue<pair<pair<int, int>, int>> q;
     memset(visited, false, sizeof(visited));
-    int a_info = area[r][c];
-    
-    queue<pair<node,int>> q;
-    q.push(make_pair(node(r,c), 0));
-    visited[r][c] = true;
-    
+    q.push({{a, b}, 0});
+    visited[a][b] = true;
+
+    int number = dist[a][b];
+
     while (!q.empty()) {
         int x = q.front().first.first;
         int y = q.front().first.second;
         int d = q.front().second;
         q.pop();
-        
-        if(area[x][y] != a_info && map[x][y] == 1){
-            ans = min(ans,d-1);
+
+        if (dist[x][y] != number && board[x][y] == 1) {
+            ans = min(ans, d - 1);
             return;
         }
-        
-        for(int i=0; i<4; i++){
+
+        for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
-            
-            if(nx<0 || nx>=n || ny<0 || ny>=n) continue;
-            
-            if((area[nx][ny]!=a_info || area[nx][ny]==-1) && !visited[nx][ny]){
-                q.push(make_pair(node(nx,ny), d+1));
+
+            if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+
+            if((dist[nx][ny]!=number || dist[nx][ny]==-1) && !visited[nx][ny]){
+                q.push({{nx,ny}, d+1});
                 visited[nx][ny] = true;
             }
         }
     }
 }
 
-int main(int argc, const char * argv[]) {
-    // cin,cout 속도향상
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-    
-    cin >> n;
-    
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            cin >> map[i][j];
+bool side(int a, int b){
+    for(int i=0; i<4;i++){
+        int nx = a + dx[i];
+        int ny = b + dy[i];
+
+        if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+
+        if(board[nx][ny]==0){
+            return true;
         }
     }
-    
-    memset(area, -1, sizeof(area));
-    
-    // 영역 번호 구하기
-    cnt = 1;
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            if(area[i][j]==-1 && map[i][j]==1){
-                bfs_area(i,j);
+    return false;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> n;
+
+    memset(dist, -1, sizeof(dist));
+
+    cnt=1;
+    for(int i=0; i<n;i++){
+        for(int j=0; j<n;j++){
+            cin >> board[i][j];
+        }
+    }
+
+    for(int i=0; i<n;i++){
+        for(int j=0; j<n;j++){
+            if(board[i][j] == 1 && dist[i][j]==-1){
+                bfs(i,j);
                 cnt++;
             }
         }
     }
-    
-    // 다리 경로 구하기
-    ans = INF;
+
+    ans = 987654321;
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
-            if(map[i][j]==1 && is_adjacentSea(i,j)){
-                bfs_dist(i,j);
+            if(board[i][j]==1 && side(i,j)){
+                bfs2(i,j);
             }
         }
     }
     cout << ans << "\n";
+
+    return 0;
 }
