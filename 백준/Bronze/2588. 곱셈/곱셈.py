3d5df3b633a39d39c3415f568bb3698fe5ab3dd9
oh2279 <85153493+oh2279@users.noreply.github.com>
2022-10-03 23:26:36 +0900

    [Bronze III] Title: 곱셈, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,14 @@
+import sys
+a = input()
+b = input()
+
+ans=0
+
+ans = int(a) * int(b[2])
+print(ans)
+ans = int(a) * int(b[1])
+print(ans)
+ans = int(a) * int(b[0])
+print(ans)
+ans = int(a) * int(b)
+print(ans)
