7a2b8b65932cd76a2bd54762111202eb80fed6ee
oh2279 <85153493+oh2279@users.noreply.github.com>
2022-11-21 00:50:07 +0900

    [Gold III] Title: Cubeditor, Time: 80 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Gold/1701. Cubeditor/Cubeditor.cc
+++ 백준/Gold/1701. Cubeditor/Cubeditor.cc
@@ -5,13 +5,13 @@ typedef unsigned long long ull;
 string s;
 int ans = -1;
 
-int failure(string str){
+int max_failure(string str){
     int j=0;
     vector<int> fail(str.size());
     // pi 배열은 접두사 접미사가 일치하는 최대 길이 구하기
     for(int i = 1; i < str.size() ; i++){
         while(j > 0 && str[i] != str[j]) { // 다르면
-            j = fail[j - 1]; // 일치할 때 까지 인덱스를 앞당김
+            j = fail[j - 1]; // 일치하는 부분까지 인덱스를 앞당김
         }
         if(str[i] == str[j]) {
             fail[i] = ++j; // 접두 접미사가 일치하는 최대 길이 저장
@@ -19,9 +19,9 @@ int failure(string str){
         else{
             fail[i]=0;
         }
+        ans = max(ans,fail[i]);
     }
-    sort(fail.begin(),fail.end());
-    return fail.back();
+    return ans;
 }
 
 int main() {
@@ -30,12 +30,13 @@ int main() {
     cout.tie(nullptr);
 
     // 과제 해석 : 문자열을 잘라보면서 그때마다의 실패함수의 최댓값을 가지고,
-    // 매번 최댓값 비교를 통해 마지막에 남는 값이 두 번 이상 나오면서 가장 긴 부분문자열의 길이이다.
+    // 매번 최댓값 비교를 통해 마지막에 남는 값이 바로, 두 번 이상 나오면서 가장 긴 부분문자열의 길이이다.
+    // (두번 이상 나온다 == (접두사==접미사))
 
     cin >> s;
 
     for(int i=0; i<s.size()-1;i++){
-        int maxi = failure(s.substr(i,s.size()));
+        int maxi = max_failure(s.substr(i));
         if(maxi >= ans){
             ans = maxi;
         }
