729d22d72de03e3dc11b70b384779bee152109f6
oh2279 <85153493+oh2279@users.noreply.github.com>
2023-01-27 16:45:32 +0900

    [Gold IV] Title: 가르침, Time: 1224 ms, Memory: 118400 KB -BaekjoonHub

---
+++ 백준/Gold/1062. 가르침/가르침.py
@@ -0,0 +1,50 @@
+import sys
+#sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+n, k= map(int, input().split())
+alpha = [False for _ in range(26)]
+words = [input().rstrip() for _ in range(n)]
+ans =0
+
+def check():
+  cnt = 0
+  for word in words:
+    flag = True
+    for s in word:
+      if not alpha[ord(s)-97]:
+        flag = False
+        break
+    if flag:
+      cnt += 1
+  return cnt
+
+def dfs(start, depth):
+  global ans
+  
+  if depth == k:
+    ans = max(ans, check())
+    return
+
+  for i in range(start, 26):
+    if not alpha[i]:
+      alpha[i] = True
+      dfs(i, depth+1)
+      alpha[i] = False
+      
+# 메인 함수
+def main():
+  if k < 5:
+    print(0)
+  elif k == 26:
+    print(n)
+  else:
+    for c in ('a', 'c', 'i', 'n', 't'):
+        alpha[ord(c) - ord('a')] = True
+
+    dfs(0, 5)
+
+    print(ans)
+
+if __name__ == '__main__':
+    main()
