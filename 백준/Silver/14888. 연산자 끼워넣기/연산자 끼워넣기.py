e2007cdf67e912815ce3fd68a2c91e61592e6657
oh2279 <85153493+oh2279@users.noreply.github.com>
2023-02-02 02:31:03 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 64 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -0,0 +1,47 @@
+import sys
+#sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+def main():  
+  dfs(1,numbers[0])
+  
+  print(max_ans,min_ans, sep='\n')
+  
+def dfs(depth,num):
+  global add, sub, mul, div, max_ans, min_ans
+  
+  if depth == n:
+    max_ans = max(max_ans,num)
+    min_ans = min(min_ans,num)
+    
+  else:
+    if add > 0:
+      add -= 1
+      dfs(depth+1,num + numbers[depth])
+      add+=1
+      
+    if sub > 0:
+      sub -= 1
+      dfs(depth+1,num - numbers[depth])
+      sub+=1
+      
+    if mul > 0:
+      mul -= 1
+      dfs(depth+1,num * numbers[depth])
+      mul+=1
+      
+    if div > 0:
+      div -= 1
+      dfs(depth+1, int(num / numbers[depth]))
+      div+=1
+      
+  
+if __name__ == "__main__":
+  n = int(input())
+  numbers = list(map(int, input().split()))
+  add, sub, mul, div = map(int, input().split())
+  
+  max_ans = -1e9
+  min_ans = 1e9
+
+  main()
