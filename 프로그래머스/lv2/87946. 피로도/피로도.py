0c5f862ef9a90e4157472c09398522137c4c9c10
oh2279 <85153493+oh2279@users.noreply.github.com>
2023-09-04 16:30:26 +0900

    [level 2] Title: 피로도, Time: 0.05 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/87946. 피로도/피로도.py
@@ -0,0 +1,17 @@
+from itertools import permutations
+
+def solution(k, dungeons):
+    answer = -1
+    
+    for order in permutations(dungeons):
+        ans = 0
+        tmp = k
+        for under, use in order:
+            if tmp >= under:
+                tmp -= use
+                ans += 1
+        
+        if ans > answer:
+            answer = ans
+    
+    return answer
